d2a8e4a3c423494be7934592ed96fd5970dde5f6
Crown <matt@crownhive.net>
2020-03-27 16:58:57 -0400

    Slots

--- SlotsUI/ContentView.swift
+++ SlotsUI/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  SlotsUI
+//  SlotsApp
 //
 //  Created by Matthew Crown on 3/27/20.
 //  Copyright © 2020 Matthew Crown. All rights reserved.
@@ -9,8 +9,125 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    private var symbols = ["apple", "star", "cherry"]
+    
+    @State private var numbers = [1,2,0]
+    @State private var credits = 1000
+    private var betAmount = 5
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            
+            //Background
+            
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            VStack {
+                
+                
+                Spacer()
+                
+                //Title
+                
+                HStack {
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    
+                    
+                    Text("SwiftUI Slots")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2)
+                
+                Spacer()
+                
+                //Credits Counter
+                Text("Credits " + "\(credits)")
+                    .foregroundColor(.black)
+                    
+                    
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                
+                Spacer()
+                
+                // Cards
+                
+                HStack {
+                    Spacer()
+                    Image(symbols[numbers[0]])
+                        .resizable().aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image(symbols[numbers[1]])
+                        .resizable().aspectRatio(1, contentMode: .fit)
+                                       .background(Color.white.opacity(0.5))
+                                       .cornerRadius(20)
+                    
+                   Image(symbols[numbers[2]])
+                    .resizable().aspectRatio(1, contentMode: .fit)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    Spacer()
+                    
+                    
+                }
+                
+                //Button
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count-1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count-1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count-1)
+                    
+                    // check winnings
+                    
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
+                        
+                        //Won
+                        
+                        self.credits += self.betAmount * 10
+                    } else {
+                        self.credits -= self.betAmount
+                        
+                    }
+                    
+                    
+                }) {
+                    Text("Spin")
+                    .bold()
+                        .foregroundColor(.white)
+                        .padding(.all,10)
+                        .padding([.leading, .trailing], 30)
+                        .background(Color.pink)
+                    .cornerRadius(20)
+                }
+                
+                Spacer()
+                
+            }
+        }
     }
 }
 
